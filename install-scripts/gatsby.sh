b3587dd1d0f221ce3cf5a652e0cddbc396abf52a
Sai-C-K <Sai.C.K.Dev@gmail.com>
2021-05-11 21:20:15 -0700

    Add gatsby support.

---
+++ install-scripts/gatsby.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+source <(curl -fsSL https://raw.githubusercontent.com/sai-c-k/env-setup/main/install-scripts/node.sh)
+
+if ! command_exists npm; then
+  echo "Please install npm."
+  exit 1
+fi
+
+if ! command_exists gatsby; then
+  echo "Installing node..."
+  npm install --global gatsby-cli
+  echo "Installation Complete: node"
+fi
